70cf9e9f74086b6175d0d1e348f3a82355e8746b
Rory Haggart <rdeh10@googlemail.com>
2021-05-29 13:16:56 +0100

    rotating solar panels based on position in orbit

--- main.py
+++ main.py
@@ -11,13 +11,28 @@ Created on Sat May 29 08:37:34 2021
     # create a dashboard type thing that allows for variable parameters
     # print orbital parameters that aren't tunable (eccentricity, ap/per, etc)
     # print velocty? altitude?
-
+    # create a 'orbital parent' class? allows us to easily switch between
+        # bodies. e.g. change distance from sun, radius, gravity, etc
+    # add the body name into parent somehow
+    
 # for plotting
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
+from matplotlib.patches import Rectangle
 from matplotlib import animation
+import matplotlib
 
 import numpy as np
+
+    
+# GLOBAL CONSTANTS
+RADIUS = 6371       # radius of parent [km] (R_Earth = 6371)
+MASS = 5.972e24     # mass of Earth [kg] (M_Earth = 5.972e24)
+GRAV = 6.67408e-11  # universal gravitational constant [m3 kg-1 s-2]
+MU = GRAV * MASS    # standard gravitational parameter of Earth [m3 s−2]
+
+COLOR = "#0088ff"
+NAME = "EARTH"
 
 def main():
     # change the backend to interactive (qt) so we can see animations
@@ -50,22 +65,18 @@ def plotOrbit(semiMajor, semiMinor, focus):
     # todo: animate the orbit - preferably keep the whole orbit visible 
         # and have the point moving around the path
     
-    # CONSTANTS
-    R_Earth = 6371          # radius of Earth [km]
-    M_Earth = 5.972e24      # mass of Earth [kg]
-    G = 6.67408e-11         # universal gravitational constant [m3 kg-1 s-2]
-    mu_Earth = G * M_Earth  # standard gravitational parameter of Earth [m3 s−2]
-    
-    
     # define earth
-    earth = Ellipse([0,0], R_Earth*2, R_Earth*2, angle=0, linewidth=1, fill=1, color="#0088ff")
+    earth = Ellipse([0,0], RADIUS*2, RADIUS*2, angle=0, linewidth=1, fill=1, color=COLOR)
     
     # define satellite orbit, with the focus located at centre of earth
         # todo: include an angle?
     orbit = Ellipse([focus,0], semiMajor*2, semiMinor*2, angle=0, linewidth=1, fill=0)
     
+    satWidth = 2000
+    satHeight = 500
+    
     # define the satellite representation (a circle)
-    satellite = plt.Circle((1000, -1000), 1000, fc='k')
+    satellite = Rectangle((0, 0), satWidth, satHeight, fc='k')
     
     # create axes
     fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
@@ -74,6 +85,9 @@ def plotOrbit(semiMajor, semiMinor, focus):
     ax.add_artist(orbit)
     ax.add_artist(earth)
     
+    labelX = -1 * RADIUS * np.sin(np.pi/4)
+    labelY = -1 * RADIUS * np.cos(np.pi/4)
+
     # make sure everything is visibile
     ax.set_xlim([-1.1*(focus + semiMajor), 1.1*(focus + semiMajor)])
     ax.set_ylim([-1.1*semiMinor, 1.1*semiMinor])
@@ -82,7 +96,7 @@ def plotOrbit(semiMajor, semiMinor, focus):
     #fig.set_size_inches(7, 6.5)
     
     rMin = np.sqrt((semiMajor - focus)**2)
-    vMax = np.sqrt(mu_Earth * (2/rMin - 1/semiMajor))
+    vMax = np.sqrt(MU * (2/rMin - 1/semiMajor))
     
     # initialise the satellite animation
     def init():
@@ -99,9 +113,12 @@ def plotOrbit(semiMajor, semiMinor, focus):
         
         # distance between satellite centre and Earth centre at this point
         r = np.sqrt(x**2 + y**2)   
+       
+        # angle between satellite and Earth centre at this point
+        theta = np.arctan(y/x)
         
         # calculate the tangential speed of the satellite at this point
-        vTangential = np.sqrt(mu_Earth * (2/r - 1/semiMajor))
+        vTangential = np.sqrt(MU * (2/r - 1/semiMajor))
         
         # normalise the speed against the point of maximum velocity
         speed = vTangential/vMax
@@ -111,7 +128,16 @@ def plotOrbit(semiMajor, semiMinor, focus):
         anim.event_source.interval = 10/speed
         
         # move the satellite to the new point on the ellipse trajectory
-        satellite.center = (x, y)
+        satellite.set_xy([x - satWidth/2, y - satHeight/2])
+    
+        ts = ax.transData
+        coords = ts.transform([x,y])
+        tr = matplotlib.transforms.Affine2D().rotate_around(coords[0],coords[1], theta - np.pi/2)
+        t= ts + tr
+    
+        satellite.set_transform(t)
+        
+        
         return satellite,
         
     # execute the animation
